3a71bc0a93a7826f97576815eff31b1467cf8a18
none <none@none.come>
2025-10-21 22:23:46 +0300

    звездочки

--- 2025.10.16_while_repeat.py/find_mind.py
+++ 2025.10.16_while_repeat.py/find_mind.py
@@ -3,3 +3,49 @@ print("Медмус")
 arr = [3,4,4,5,1]
 print("жто минимум", min(arr))
 print("жто минимум", min(arr))
+print('1\n2\n3\n4\n5\n')
+print(' 1\n','2\n','3\n','4\n','5\n','')
+
+a = "привет"
+b = "пока"
+с = "там"
+print(a) 
+
+print(len('ftrfgfhfh'))
+print("(=^.^=)" * 50)
+print('a'*1980)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+print("ш"*10)
+x = 0
+while x < 10:
+    print("ш"*10)
+    x += 1
+x = 0
+a = '.'
+b = '0'
+
+# while x < 5:
+#     print((a+b)*10)
+#     print((b+a)*10)
+#     x += 1
+
+# while x < 5:
+#     print((a*3 +"\n"*3+b*3)*10)
+#     print((b*3+a*3)*10)
+#     x += 1 
+for i in range(3):
+    for i in range(3):
+        print((((("*"*3) +'\n')*3) + ((('0'*3) + '\n')*3))*3)
+# '\n' + ('0'*3 + "*"*3)*3)
+        
+ 
+
+
