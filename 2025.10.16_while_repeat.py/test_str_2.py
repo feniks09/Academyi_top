7002e6df86f81dbf1033c28e8ad6ed56e514f01a
Aleksey <Feniks09@mail.ru>
2025-10-22 18:07:30 +0300

    22.10_18:00

---
+++ 2025.10.16_while_repeat.py/test_str_2.py
@@ -0,0 +1,12 @@
+""" х должно быть числом"""
+x = 10
+def espero():
+    """x должно быть числом"""
+    print("Напечатали", x)
+
+espero()
+
+start_ = "привет"
+finish_ = "Пока"
+print(start_ + " "*4 + finish_)
+print("\n".join([start_, finish_]))
