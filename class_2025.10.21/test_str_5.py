2c3ae1e10896043b6c11fbe1a4a26c5f5fefab1c
feniks09 <feniks09@mail.ru>
2025-11-05 07:32:28 +0300

    документ строка

---
+++ class_2025.10.21/test_str_5.py
@@ -0,0 +1,39 @@
+a = 89
+       # это переменная
+    # которой присвоили целое число
+  # комментарии после неё
+b = 89
+a = 12 
+"а это - синтаксическая ошибка:"
+"это строка, которая располагается в коде"
+"где попало\", нарушая законы индентации"
+"Проверено на питоне 3.1 \
+    "
+a = """sdcdsfcdss
+ewfcewfcewfcwe d d d d d  d d d d d d d d d  d sd
+ewfes s s s s  s s sd d s d d d 
+wdfwessss s  sd s  s
+d s s s  s s 
+weds s s s 
+weewdwedwed
+w
+ew
+we
+""".split(" " \
+"" \
+"")
+"ушцаоуцло"
+"sdcndscfn"
+print(a)
+
+
+a = "wee"
+b = 'wee'
+c ="""wee"""
+d = '''wee'''
+
+print(a == b, a == c, a == d, sep="\n")
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
