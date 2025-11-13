1a639e0af4cf7fdd0ecd2498af1683522049cb40
none <none@none.come>
2025-11-13 22:27:40 +0300

    класная работа 13.11.2025 лист

---
+++ class_2025.11.13__list_tuple.py/metod_list.py
@@ -0,0 +1,17 @@
+list_1 = ["yerh", "wefew", "ewfwe", "dfervg"]
+
+# del list_1[0]
+# print(list_1)
+
+list_1.remove("yerh")
+
+print(list_1)
+
+
+for index, value in enumerate(list_1):
+    print(index, value)
+
+
+list_1.insert(123456789012345678, "****")
+print(list_1)
+
---
+++ class_2025.11.13__list_tuple.py/task_1.py
@@ -0,0 +1,26 @@
+# 1. Задвоить элементы в списке. 
+# То есть компьютер превращает список 
+# [4, print, "xyz", None] в список 
+# [4, 4, print, print, "xyz", "xyz", None, None]
+
+list_1 = [4, print, "xyz", None]
+list_2 = []
+# count = 0
+# i = 0
+# for i in list_1:
+#     i +=1
+#     list_1.insert(count, i)
+#     count += 1
+
+# print(list_1)
+# list_1.insert(0, 4)
+# list_1.insert(2, print)
+# list_1.insert(4, "xyz")
+# list_1.insert(6, None)
+count = 0
+for list_ in list_1:
+    list_2.append(str(list_)*2)
+    
+
+
+print(list_2)
