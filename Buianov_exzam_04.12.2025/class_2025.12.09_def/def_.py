c025463e011e9e608e7f5fc48a7a5c88ba73c58b
Feniks09 <Feniks09@mail.ru>
2025-12-10 13:50:06 +0300

    структуры данных словари

---
+++ Buianov_exzam_04.12.2025/class_2025.12.09_def/def_.py
@@ -0,0 +1,14 @@
+def print_derecs():
+    print(" ^ ")
+    print("< >")
+    print(" v ")
+
+print_derecs()
+
+def mnoj(a, b):
+    print(a * b)
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+mnoj(a, b)
