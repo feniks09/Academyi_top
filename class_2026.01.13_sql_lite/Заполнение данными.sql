f53571412b554572d02229ab0f989d65cfa1c944
none <none@none.come>
2026-01-13 22:18:14 +0300

    занятие от 13.01.26 sqlite связаных таблиц

---
+++ class_2026.01.13_sql_lite/Заполнение данными.sql
@@ -0,0 +1,12 @@
+DELETE from "Album";
+delete from "Band";
+
+insert into "Band"
+	("name", "year_of_creation")
+VALUES 
+	('Звукачи', 1980);
+INSERT into "Album"
+    ("name", "year_of_creation", "track_count", "band_id")
+
+VALUES
+	('Ого-го', 1987, 10, 1);
