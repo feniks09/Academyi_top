50550bbef8764d48da002cea3cf1e1ab05cb741b
Feniks09 <Feniks09@mail.ru>
2026-05-12 19:36:20 +0300

    дз от 30.04.2026 js декарт сист координат

--- dz_Buyanov_2026.04.30_js_decert_syst/decart_sys.js
+++ dz_Buyanov_2026.04.30_js_decert_syst/decart_sys.js
@@ -12,32 +12,49 @@ function defound()
 
     if (x > 0 && y > 0) 
         {
-        let rez = `Первая четверть`
+        let rez = `Координаты находятся в Первой четверти`
         output.innerText = rez
         }
     
     else if (x < 0 && y > 0)
         {
-        let rez = `Вторая четверть`
+        let rez = `Координаты находятся в второй четверти`
         output.innerText = rez
         }
     
     else if (x < 0 && y < 0)
         {
-            let rez = `Третья четверть`
+            let rez = `Координаты находятся в третьей четверти`
             output.innerText = rez
         }
     
     else if (x > 0 && y < 0)
         {
-            let rez = `Четвертая четверть`
+            let rez = `Координаты находятся в четвертой четверти`
             output.innerText = rez
         }
-    else if (x === 0 && y > 0)
+    else if ((x === 0 && y > 0) || (x === 0 && y < 0))
         {
-            let rez = `Координаты находятся на оси Х`
+            let rez = `Координаты находятся на оси Y`
             output.innerText = rez
         }
+
+    // else if (x === 0 && y < 0)
+    //     {
+    //         let rez = `Координаты находятся на оси отрицательной части оси Y`
+    //         output.innerText = rez
+    //     }
+    else if ((x > 0 && y === 0) || x < 0 && y === 0)
+        {
+            let rez = `Координаты находятся на оси X`
+            output.innerText = rez
+        }
+    
+    // else if (x < 0 && y === 0)
+    //     {
+    //         let rez = `Координаты находятся на отрицательной части оси X`
+    //         output.innerText = rez
+    //     }    
     else 
         {
            output.innerText = `Вы ввели не число` 
