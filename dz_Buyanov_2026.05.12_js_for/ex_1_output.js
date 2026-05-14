ce4af8e5b2fd5cdd352ec20ab5c6562080c8d773
Aleksey <Feniks09@mail.ru>
2026-05-14 09:58:57 +0300

    дз от 12.05.26 структура

---
+++ dz_Buyanov_2026.05.12_js_for/ex_1_output.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function main()
+{
+    const input = document.querySelector("#input")
+
+    const output = document.querySelector("#output")
+
+    let num = parseFloat(input.value)
+
+    output.innerText = num
+}
