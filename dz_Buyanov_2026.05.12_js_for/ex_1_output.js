e1b3ed68a3d9c37eb45246c168c36333d2759ed2
Aleksey <Feniks09@mail.ru>
2026-05-18 17:30:41 +0300

    назначение ДЗ от 04.23.26

--- dz_Buyanov_2026.05.12_js_for/ex_1_output.js
+++ dz_Buyanov_2026.05.12_js_for/ex_1_output.js
@@ -4,9 +4,66 @@ function main()
 {
     const input = document.querySelector("#input")
 
-    const output = document.querySelector("#output")
+    const outputFor = document.querySelector("#output_for")
+    const outputWhile = document.querySelector("#output_while")
+    const outputDoWhile = document.querySelector("#output_do_while")
 
     let num = parseFloat(input.value)
+    let text = ''
+    let text2 = ''
+    let text3 = "";
+    let i = 0;
+    let n = 0;
 
-    output.innerText = num
+    if (num >= 0)
+    {
+    for (let i = 0; i <= num; i++)
+    {
+        text += `${i} `
+    }
+
+    while (i <= num)
+    {
+        text2 += `${i} `
+        i++
+    }
+
+    do
+    {
+        text3 += `${n} `
+        n++
+    }
+    while (n <= num)
+    }
+
+    else if (num < 0)
+    {
+        for (let i = 0; i > num; i--)
+        {
+            text += `${i} `
+        }
+
+        while (i > num)
+        {
+            text2 += `${i} `
+            i--
+        }
+
+        do
+        {
+            text3 += `${n} `
+            n--
+        }
+        while (n > num)
+    }
+    else
+    {
+        text = "Введено не число"
+        text2 = "Введено не число" 
+        text3 = "Введено не число"  
+
+    }
+    outputFor.innerText = text
+    outputWhile.innerText = text2
+    outputDoWhile.innerText = text3
 }
