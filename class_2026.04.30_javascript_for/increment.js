cba626bd83ee9b82a66482ea27c2a287d1462640
Aleksey <Feniks09@mail.ru>
2026-05-18 17:33:01 +0300

    test js increment

---
+++ class_2026.04.30_javascript_for/increment.js
@@ -0,0 +1,62 @@
+// "use strict"
+// let i = 7
+// let num = i++
+// console.log(i, num)
+// let num2 = ++i
+
+// console.log(i, num)
+// console.log(i, num2)
+
+// for (let i = 0; i < 10; i++)
+// {
+//     console.log(i)
+// }
+
+// for (let i = 0; i > -10; i--)
+// {
+//     console.log(i)
+// }
+// console.log("цикл while")
+// let num3 = 3
+// let n = 14
+// while (num3 < n)
+// {
+//     console.log(num3)
+//     num3++
+//     console.log(num3)
+    
+// }
+
+
+
+// let name = 10
+// let rez = 2
+
+// do{
+//     console.log("цикл do while", name)
+//     name--
+// }
+// while (name > rez)
+
+
+
+
+
+
+
+
+
+let c = 10
+// console.log(c)
+// let z = c++
+// console.log(c)
+// let y = ++c
+// console.log(c, z, y)
+console.log(++c)
+console.log(c++)
+console.log(--c)
+console.log(c--)
+console.log(c)
+
+
+
