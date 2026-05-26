e3698820d8a2e96ac0225b7e5d92b49f3958734d
Feniks09 <Feniks09@mail.ru>
2026-05-26 07:45:03 +0300

    дз от 19.05.26 js addCard без функций

--- dz_Buyanov_2026.05.19_addCard/addCard2.js
+++ dz_Buyanov_2026.05.19_addCard/addCard2.js
@@ -10,37 +10,43 @@ document.addEventListener("DOMContentLoaded", () =>
     const buttonAddDocAllComlect = document.querySelector("#buttonAddDocumentAllComplect");
     buttonAddDocAllComlect.addEventListener('click', () =>
     {
+        if (inputNameDocumentALL.value.trim() === "") return;
         const pDocument = document.createElement("p");
         pDocument.textContent = inputNameDocumentALL.value;
-        // buttonRemoveDocument.addEventListener("click", () =>
-        // {
-        //     card.removeChild(pDocument)
-        // })
+        const buttonRemoveDocument = document.createElement("button")
+        buttonRemoveDocument.textContent = "X1"
+        buttonRemoveDocument.classList.add("btnRemoveDocument")
         pDocument.appendChild(buttonRemoveDocument)
+
         const AllCard = document.querySelectorAll(".card")
         AllCard.forEach(card => 
-            {
-                card.appendChild(pDocument.cloneNode(true))
-                const buttonRemoveDocument = document.createElement("button")
-                buttonRemoveDocument.textContent = "X"
-                buttonRemoveDocument.addEventListener("click", () =>
-                {
-                    card.removeChild(pDocument)
-                })
+            {               
+               const cloneDocum = pDocument.cloneNode(true)
+               const cloneButton = cloneDocum.querySelector(".btnRemoveDocument")
+               cloneButton.addEventListener("click", () => 
+                  {
+                      card.removeChild(cloneDocum)
+                  })
+               card.appendChild(cloneDocum)
+               inputNameDocumentALL.value = ""
+                
             })
+
     })
 
     const output = document.querySelector("#output")
 
     buttonAddComplectDoc.addEventListener("click", () =>
     {
+        if (inputNameComplect.value.trim() === "") return;
         const card = document.createElement("div")
         card.classList.add("card")
         const titleCard = document.createElement("p")
         titleCard.classList.add("titleCard")
         titleCard.textContent = inputNameComplect.value
+        inputNameComplect.value = ""
         const buttonRemoveCard = document.createElement("button")
-        buttonRemoveCard.textContent = "X"
+        buttonRemoveCard.textContent = "X2"
         buttonRemoveCard.addEventListener("click", () => 
         {
             output.removeChild(card)
@@ -51,10 +57,12 @@ document.addEventListener("DOMContentLoaded", () =>
         buttonAddDocumentToComplect.textContent = "Добавить документ"
         buttonAddDocumentToComplect.addEventListener("click", () =>
         {
+            if (inputNameDocument.value.trim() === "") return;
             const pDocument = document.createElement("p");
             pDocument.textContent = inputNameDocument.value;
             const buttonRemoveDocument = document.createElement("button")
-            buttonRemoveDocument.textContent = "X"
+            buttonRemoveDocument.textContent = "X3"
+            inputNameDocument.value = ""
             buttonRemoveDocument.addEventListener("click", () =>
             {
                 card.removeChild(pDocument)
