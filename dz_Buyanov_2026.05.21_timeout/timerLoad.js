632b578c751234f6bf144aa53b2396a969a587c4
Aleksey <Feniks09@mail.ru>
2026-06-02 17:54:01 +0300

    дз тяймаут редакция

--- dz_Buyanov_2026.05.21_timeout/timerLoad.js
+++ dz_Buyanov_2026.05.21_timeout/timerLoad.js
@@ -5,41 +5,62 @@ document.addEventListener("DOMContentLoaded", () =>
 {
     const output = document.querySelector("#output")
     const button = document.querySelector("#button")
-    let counter = 100;
-    function creatButter()
+    let newX = 100;
+    function creatConteinButter()
     {
         const butter = document.createElement("div")
-        const line = document.createElement("p")
-
+        butter.className = "butterContein"
         butter.style.width = "100px";
         butter.style.height = "50px";
         butter.style.border = "2px solid blue";
         butter.style.borderRadius = "10px";
-        butter.style.display = block;
+        butter.style.display = 'block';
         butter.style.position = "relative"
 
-        line.style.width = "10px";
-        line.style.height = "50px";
-        line.style.border = "2px solid green"
-        line.style.position = "absolute"
-        // butter.style.backgroundColor = 'blue'
-
-        addButter(butter)
-        addLine(butter, line)
+        addButterInOutput(butter)
     }
 
-    
-    creatButter()
+    function createMarcEnerg()
+    {
+        const lineEnerg = document.createElement("p")
+        const divButter = document.querySelector(".butterContein")
+        
 
-    function addButter (butter)
+        lineEnerg.style.width = "0.5px";
+        lineEnerg.style.height = "50px";
+        lineEnerg.style.border = "0.5px solid green";
+        lineEnerg.style.backgroundColor = "green";
+        lineEnerg.style.display = "block";
+        lineEnerg.style.position = "absolute";
+        lineEnerg.style.marginTop = 0
+        lineEnerg.style.left = `${newX}px`
+        addLineEnergInButtter(divButter, lineEnerg)
+    }
+
+    function modifyCoordMarEng()
+    {
+        if (newX < 0) return;
+        createMarcEnerg();
+        newX--;
+    }
+    function addButterInOutput (butter)
     {
         output.appendChild(butter)
     }
 
-    function addLine(butter, line)
+    function addLineEnergInButtter(divButter, line)
     {
-        butter.appendChild(line)
+        divButter.appendChild(line)
     }
+
+    function addLineEnergeForTimeOut()
+    {  
+        let timeOut = setInterval(modifyCoordMarEng, 50)   
+    }
+
+
+    creatConteinButter()
+    addLineEnergeForTimeOut()
+
+
 })
-
-
