5052cbace832e254a23c454af44f516ebaef527b
Feniks09 <Feniks09@mail.ru>
2026-06-03 20:03:28 +0300

    класс от 02.06.26 storeg

---
+++ class_2026.06.02_js/ex_1.js
@@ -0,0 +1,30 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    const outputInfo = document.querySelector("#outputInfo");
+    const input = document.querySelector("#input");
+    const buttonSave = document.querySelector("#buttonSave");
+    const buttonLoad = document.querySelector("#buttonLoad");
+    const LOCAL_STORAGE_KEY = "dhewyrghvbedj";
+    function save()
+    {
+        let textAsJSON = JSON.stringify(input.value);
+        localStorage.setItem(
+            LOCAL_STORAGE_KEY,
+            textAsJSON
+        );
+
+    }
+
+    function load()
+    {
+        let textAsJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (textAsJSON !== null);
+        {
+            input.value = JSON.parse(textAsJSON);
+        }
+    }
+    buttonSave.addEventListener("click", save);
+    buttonLoad.addEventListener("click", load);
+});
