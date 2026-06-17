92e1fd9a0c80c2284db456d0659f3594ea5b0f55
Aleksey <Feniks09@mail.ru>
2026-06-17 17:55:56 +0300

    доработка fetch запросов на сайт погоды

--- class_2026.06.16_js/weather.js
+++ class_2026.06.16_js/weather.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () =>
                 method : "GET",
                 headers : 
                 {
-                    "X-Api-Key" : "NfRUPXx5PfCZnltlj48dGbqTIMQNV6wycdP8gGvl"
+                    "X-Api-Key" : "bDdArAV6qhKl9wjEykr9lE2RRuIZ2kRO0GFET8O5"
                 } 
             }
         ).then(response => 
@@ -46,20 +46,60 @@ document.addEventListener("DOMContentLoaded", () =>
         }
         )
     }
-    function cityName()
+    function geolocationDate()
     {
-        let countryCode = inputCountryCode.value.trim().toUpperCase();
-        fetch("https://api.api-ninjas.com/v1/geocoding?city=moscow&country=ru",
+        let countryCode = inputCountryCode.value.trim().toUpperCase()
+        let cityName = inputCityName.value.trim().toUpperCase()
+        fetch(`https://api.api-ninjas.com/v1/geocoding?city=${encodeURIComponent(cityName)}&country=${encodeURIComponent(countryCode)}`,
             {
                 method : "GET",
                 headers : {
-                    "X-Api-Key" : "NfRUPXx5PfCZnltlj48dGbqTIMQNV6wycdP8gGvl"
+                    "X-Api-Key" : "bDdArAV6qhKl9wjEykr9lE2RRuIZ2kRO0GFET8O5"
                 }
             }
-        ).then()
+        ).then(response =>
+        {
+            if (response.ok)
+            {
+                return response.json();
+            }
+            else
+            {
+                throw new Error(`следующая ошибка ${response.status}`)
+            }
+        }).then(geolocation =>
+        {
+            if(geolocation.length > 0)
+            {
+                let geoDate = geolocation[0];
+                const keys = Object.keys(geoDate)
+                let cityName = geoDate.name;
+                let latitude = geoDate.latitude;
+                let longitude = geoDate.longitude;
+                let country = geoDate.country;
+                let state = geoDate.state; 
+                console.log(keys)
+                console.log(geolocation)
+                console.log(cityName)
+                console.log(latitude)
+                console.log(longitude)
+                console.log(country)
+                console.log(state)
+
+                output.innerHTML = `<p>${cityName}</p>
+                                    <p>${latitude}</p>
+                                    <p>${longitude}</p>
+                                    <p>${country}</p>
+                                    <p>${state}</p>
+                                    <p>${keys}</p>
+                                    `
+            }
+        })
     }
-    button.addEventListener("click", fetchWeather)
-
-
-
+    button.addEventListener("click", ()=>
+    {
+        geolocationDate()
+    })
 })
+
+
