e5b7718b69d7a40781ae6f86167f4f94845ef1f7
Feniks09 <Feniks09@mail.ru>
2026-06-23 07:53:27 +0300

    радакция внешнего выда карточки для видео

---
+++ dz_Buyanov_2026.06.16_videoPage/videoPage.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    async function queryToSearchFilm()
+    {
+        const response = await fetch()
+        {
+            
+        }
+    }
+})
