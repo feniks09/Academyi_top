752afea48aab38426fffa79716c66672a76d216d
Feniks09 <Feniks09@mail.ru>
2026-06-24 08:58:15 +0300

    редакция

--- dz_Buyanov_2026.06.16_videoPage/videoPage.js
+++ dz_Buyanov_2026.06.16_videoPage/videoPage.js
@@ -6,10 +6,16 @@ document.addEventListener("DOMContentLoaded", () =>
     async function queryToSearchFilm()
     {
         const response = await fetch(
+            "https://api.poiskkino.dev/v1.4/movie?year=2023&genres.name=криминал",
+            {
+                methed: "GET",
+                headers: 
+                {"X-API-KEY" : 5AY8CN9-635M89B-QVNAJS0-9GB0MZG"}
+            }
             
         )
         {
-            
+
         }
     }
 })
