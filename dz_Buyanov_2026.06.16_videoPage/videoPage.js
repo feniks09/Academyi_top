c13370d9e5fb9dcc6c13dd7d9097b8cb4da69abd
Aleksey <Feniks09@mail.ru>
2026-06-25 18:02:35 +0300

    дз по создание страницы с информацией о фильмах настроен fetch запрос для получения информации

--- dz_Buyanov_2026.06.16_videoPage/videoPage.js
+++ dz_Buyanov_2026.06.16_videoPage/videoPage.js
@@ -3,6 +3,8 @@
 
 document.addEventListener("DOMContentLoaded", () =>
 {
+    const output = document.querySelector("#output")
+    const button = document.querySelector("#searchButton")
     async function queryToSearchFilm()
     {
         const response = await fetch(
@@ -10,12 +12,25 @@ document.addEventListener("DOMContentLoaded", () =>
             {
                 methed: "GET",
                 headers: 
-                {"X-API-KEY" : 5AY8CN9-635M89B-QVNAJS0-9GB0MZG"}
-            }
-            
-        )
+                {"X-API-KEY" : "5AY8CN9-635M89B-QVNAJS0-9GB0MZG"}
+            })
+        const text = await response.json()
+        // let textJ = JSON.stringify(text.docs[0].countries[0].name)
+        let textList = []
+        for(let i = 0; i < text.docs.length; i++)
         {
-
+            let textI = text.docs[i]
+            let textF = JSON.stringify(textI)
+            const p = `<p>${textF}</p>`
+            textList.push(p)
         }
+        for(let p of textList)
+        {
+            output.innerHTML += p
+        }
+        // const p = `<p>${textJ}</p>`
+        // output.innerHTML = p
+        console.log(text)
     }
+    button.addEventListener("click", queryToSearchFilm)
 })
