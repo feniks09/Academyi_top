786fcb8fa8c35d47994d6e1c568834961c5faa02
Feniks09 <Feniks09@mail.ru>
2026-06-26 08:54:31 +0300

    организован вывод списка названий фильмов

--- dz_Buyanov_2026.06.16_videoPage/videoPage.js
+++ dz_Buyanov_2026.06.16_videoPage/videoPage.js
@@ -14,23 +14,30 @@ document.addEventListener("DOMContentLoaded", () =>
                 headers: 
                 {"X-API-KEY" : "5AY8CN9-635M89B-QVNAJS0-9GB0MZG"}
             })
-        const text = await response.json()
-        // let textJ = JSON.stringify(text.docs[0].countries[0].name)
-        let textList = []
-        for(let i = 0; i < text.docs.length; i++)
+        if (response.ok)
         {
-            let textI = text.docs[i]
-            let textF = JSON.stringify(textI)
-            const p = `<p>${textF}</p>`
-            textList.push(p)
+            console.log(response);
+            const text = await response.json();
+            // let textJ = JSON.stringify(text.docs[0].countries[0].name)
+            let textList = []
+            for(let i = 0; i < text.docs.length; i++)
+            {
+                let textNF = text.docs[i].alternativeName
+                const p = `<p>${i+1}.  ${textNF}</p>`
+                textList.push(p)
+            }
+            for(let p of textList)
+            {
+                output.innerHTML += p
+            }
+            // const p = `<p>${textJ}</p>`
+            // output.innerHTML = p
+            console.log(text)
         }
-        for(let p of textList)
+        else
         {
-            output.innerHTML += p
+            
         }
-        // const p = `<p>${textJ}</p>`
-        // output.innerHTML = p
-        console.log(text)
     }
     button.addEventListener("click", queryToSearchFilm)
 })
