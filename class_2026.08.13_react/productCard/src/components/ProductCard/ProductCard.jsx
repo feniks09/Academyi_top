4940abc88a126a48bbe570bb24e3ca68a9b4ebdf
Feniks09 <Feniks09@mail.ru>
2026-08-19 08:53:33 +0300

    редакция

--- class_2026.08.13_react/productCard/src/components/ProductCard/ProductCard.jsx
+++ class_2026.08.13_react/productCard/src/components/ProductCard/ProductCard.jsx
@@ -2,23 +2,34 @@ import { useState } from "react";
 
 export const ProductCard = () => {
     const [count, setCount] = useState(0)
-    const [color, setColor] = useState('blue')
+    const [color, setColor] = useState('grey')
+    const [text, setText] = useState('Купить')
+    const [status, setStatus] = useState('Показать')
     const [isExpanded, setExpanded] = useState(true);
     const [isAvailable, setAvailable] = useState(true);
     console.log(count)
-    const handleClick = () => {setCount(prev => prev + 1)
+    // setColor(isAvailable ? 'green': 'grey');
+    // setText(isAvailable ? 'Купить' : 'Нет в наличии');
+    
+    const handleClick = () => {setAvailable(!isAvailable);
+        setColor(isAvailable ? 'green': 'grey')
+
+    const handleClickDescrip = () => { setStatus(status === 'Показать' ? 'Скрыть' : 'Показать')}
     }
         return (
         <div>
-            Привет я карточка товара - {count}
+            <h1>название товара</h1>
+            <button onClick={handleClickDescrip}>{status}</button>
+            <p style={{display: false}}>описание товара</p>
             <br />
             <button onClick={handleClick}
+                    disabled={!isAvailable}
                     style = {{
                                 backgroundColor: color,
                                 color: '',
-                                borderRadius: 5
+                                borderRadius: 5,
                             }}
-                    >нажми на меня
+                    >{text}
             </button>
         </div>
     )
